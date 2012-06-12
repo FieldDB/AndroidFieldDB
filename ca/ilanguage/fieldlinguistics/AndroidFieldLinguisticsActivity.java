6a7d598ddaa80780f8853b160c65e0cfb8d021e3
trisapeace <trisapeace@gmail.com>
2012-06-12 13:06:11 -0400

    Removed unnecessary import.

--- ca/ilanguage/fieldlinguistics/AndroidFieldLinguisticsActivity.java
+++ ca/ilanguage/fieldlinguistics/AndroidFieldLinguisticsActivity.java
@@ -14,7 +14,6 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class AndroidFieldLinguisticsActivity extends Activity {
