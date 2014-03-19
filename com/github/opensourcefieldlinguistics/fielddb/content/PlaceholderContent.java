b3787994f6695b64e658b14fe96698076c0ba3a1
cesine <cesine@yahoo.com>
2014-03-19 19:37:45 +0400

    made the text transparent on the image

--- com/github/opensourcefieldlinguistics/fielddb/content/PlaceholderContent.java
+++ com/github/opensourcefieldlinguistics/fielddb/content/PlaceholderContent.java
@@ -25,9 +25,9 @@ public class PlaceholderContent {
 
     static {
         // Add 3 sample items.
-        addItem(new DummyItem("1", "Item 1"));
-        addItem(new DummyItem("2", "Item 2"));
-        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("1", "Lore ipsum wie oienoiwaenoa oaiwneoiawneoiajeow"));
+        addItem(new DummyItem("2", "Lorem"));
+        addItem(new DummyItem("3", "Eioin iwaoin aionwea oiwe ew"));
     }
 
     private static void addItem(DummyItem item) {
