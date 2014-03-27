972cc9243bb493fcc0d697725fcbbfb01fb42667
cesine <cesine@yahoo.com>
2014-03-27 14:57:36 +0400

    added play button and videoview

--- com/github/opensourcefieldlinguistics/fielddb/content/PlaceholderContent.java
+++ com/github/opensourcefieldlinguistics/fielddb/content/PlaceholderContent.java
@@ -29,7 +29,7 @@ public class PlaceholderContent {
 		// "Lorem ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow  ipsum wie oienoiwaenoa oaiwneoiawneoiajeow"));
 		Datum hello = new Datum("გამარჯობა", "gamardʒoba", "hello", "Hello",
 				"(Standard greeting)");
-		hello.addImage("gamardZoba.jpg");
+		hello.addImageFile("gamardZoba.jpg");
 		addItem(hello);
 
 		addItem(new Datum("არა მადლობა", "ara madloba", "no.thanks",
@@ -39,13 +39,13 @@ public class PlaceholderContent {
 
 		Datum howMuch = new Datum("რა ღირს?", "ra ɣirs es?", "what worth is",
 				"How much is this?");
-		howMuch.addImage("ra_ghirs_es.jpg");
+		howMuch.addImageFile("ra_ghirs_es.jpg");
 		addItem(howMuch);
 
 		Datum repeat = new Datum("გთხოვთ გაიმეორეთ სამჯერ",
 				"ktχoft gaimeorɛt samdʒɹ", "please repeat 3.times",
 				"Please repeat 3 times?");
-		repeat.addImage("ktxoft_gaimeorets_samdZr.jpg");
+		repeat.addImageFile("ktxoft_gaimeorets_samdZr.jpg");
 		addItem(repeat);
 
 		addItem(new Datum("გაიმეორეთ გთხოვთ", "gaimeorɛt ktχoft",
@@ -53,7 +53,7 @@ public class PlaceholderContent {
 
 		Datum dontHave = new Datum("არ მაქვს", "ar markʰfs", "no have",
 				"I don't have any", "(to begging children).");
-		dontHave.addImage("ar_makfs.jpg");
+		dontHave.addImageFile("ar_makfs.jpg");
 		addItem(dontHave);
 	}
 
