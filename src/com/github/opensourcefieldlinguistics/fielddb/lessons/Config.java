63b264c04db6466381b48de57169759538469dfa
cesine <cesine@yahoo.com>
2014-03-30 17:35:31 +0400

    added download sample data connections

--- src/com/github/opensourcefieldlinguistics/fielddb/lessons/Config.java
+++ src/com/github/opensourcefieldlinguistics/fielddb/lessons/Config.java
@@ -1,14 +1,18 @@
 package com.github.opensourcefieldlinguistics.fielddb.lessons;
 
 public class Config extends ca.ilanguage.oprime.Config {
-	public static String USER_FRIENDLY_DATA_NAME = "ფრაზა";
-	public static String DATA_IS_ABOUT_LANGUAGE_ISO = "ka";
-	public static String DATA_IS_ABOUT_LANGUAGE_NAME = "ქართული";
-	public static final String DEFAULT_OUTPUT_DIRECTORY = "/sdcard/FieldDB";
-
-	public static final String ACRA_USER = PrivateConstants.ACRA_USER;
 	public static final String ACRA_PASS = PrivateConstants.ACRA_PASS;
-	public static String DEFAULT_SERVER_URL = PrivateConstants.DEFAULT_SERVER_URL;
-	public static String ACRA_SERVER_URL = PrivateConstants.ACRA_SERVER_URL;
+	public static final String ACRA_SERVER_URL = PrivateConstants.ACRA_SERVER_URL;
+	public static final String ACRA_USER = PrivateConstants.ACRA_USER;
+	public static final String DATA_IS_ABOUT_LANGUAGE_ISO = "ka";
+	public static final String DATA_IS_ABOUT_LANGUAGE_NAME = "ქართული";
+
+	public static final String DEFAULT_CORPUS = "community-georgian";
+	public static final String DEFAULT_OUTPUT_DIRECTORY = "/sdcard/FieldDB";
+	public static final String DEFAULT_SERVER_URL = PrivateConstants.DEFAULT_SERVER_URL;
+	public static final String DEFAULT_SAMPLE_DATA_URL = PrivateConstants.DEFAULT_SERVER_URL
+			+ "/" + DEFAULT_CORPUS + "/_design/learnx/views/byTag?key=";
+
 	public static final String KEYSTORE_PASS = PrivateConstants.KEYSTORE_PASS;
+	public static final String USER_FRIENDLY_DATA_NAME = "ფრაზა";
 }
--- src/com/github/opensourcefieldlinguistics/fielddb/service/DownloadDatumsService.java
+++ src/com/github/opensourcefieldlinguistics/fielddb/service/DownloadDatumsService.java
@@ -1,11 +1,32 @@
 package com.github.opensourcefieldlinguistics.fielddb.service;
 
-import org.acra.ACRA;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
-import ca.ilanguage.oprime.Config;
+import org.acra.ACRA;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import ca.ilanguage.oprime.datacollection.SecureHttpClient;
+
+import com.github.opensourcefieldlinguistics.fielddb.lessons.Config;
+import com.github.opensourcefieldlinguistics.fielddb.lessons.georgian.R;
+
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 public class DownloadDatumsService extends IntentService {
 
@@ -24,12 +45,97 @@ public class DownloadDatumsService extends IntentService {
 		}
 
 		int notificationId = (int) System.currentTimeMillis();
-		// String uploadStatusMessage = getString(R.string.preparing_upload);
-		String tag = "SampleData";
-		ACRA.getErrorReporter().putCustomData("downloadDatums", tag);
+		String uploadStatusMessage = "Downloading sample "
+				+ Config.USER_FRIENDLY_DATA_NAME;
+
+		// tryUploadAgain
+		Intent tryUploadAgain = new Intent(this, DownloadDatumsService.class);
+
+		PendingIntent pIntent = PendingIntent.getService(this, 323813,
+				tryUploadAgain, Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+		// NOTIFICATION
+		RemoteViews notificationView = new RemoteViews(getPackageName(),
+				R.layout.notification);
+		notificationView.setTextViewText(R.id.notification_text,
+				"Preparing download");
+		notificationView
+				.setTextViewText(R.id.notification_title, "Downloading");
+		Notification noti = new NotificationCompat.Builder(this)
+				.setTicker(uploadStatusMessage).setContent(notificationView)
+				.setSmallIcon(R.drawable.ic_oprime).setContentIntent(pIntent)
+				.build();
+		noti.flags = Notification.FLAG_AUTO_CANCEL;
+		notifyUser(uploadStatusMessage, noti, notificationId, false);
+
+		String datumTagToDownload = "SampleData";
+		ACRA.getErrorReporter().putCustomData("downloadDatums",
+				datumTagToDownload);
 		ACRA.getErrorReporter().handleException(
 				new Exception("*** Download Data Started ***"));
 
+		SecureHttpClient httpClient = new SecureHttpClient(
+				getApplicationContext());
+		httpClient.setKeystoreIdandPassword(R.raw.sslkeystore,
+				Config.KEYSTORE_PASS);
+
+		HttpContext localContext = new BasicHttpContext();
+		HttpGet httpGet = new HttpGet(Config.DEFAULT_SAMPLE_DATA_URL
+				+ datumTagToDownload);
+		MultipartEntity entity = new MultipartEntity(
+				HttpMultipartMode.BROWSER_COMPATIBLE, null,
+				Charset.forName("UTF-8"));
+
+		String userFriendlyErrorMessage = "";
+		uploadStatusMessage = "Contacting server";
+		notifyUser(uploadStatusMessage, noti, notificationId, false);
+
+		HttpResponse response;
+		try {
+			response = httpClient.execute(httpGet, localContext);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent(), "UTF-8"));
+			uploadStatusMessage = "Server contacted";
+			notifyUser(uploadStatusMessage, noti, notificationId, false);
+			String JSONResponse = "";
+			String newLine;
+			do {
+				newLine = reader.readLine();
+				if (newLine != null) {
+					JSONResponse += newLine;
+				}
+			} while (newLine != null);
+			Log.d(Config.TAG, JSONResponse);
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		/*
+		 * Displays enotificationManagerrror if exists upon upload, otherwise
+		 * cancels the notification
+		 */
+		if (!"".equals(userFriendlyErrorMessage)) {
+			notifyUser(" " + userFriendlyErrorMessage, noti, notificationId,
+					true);
+		} else {
+			/* Success: remove the notification */
+			((NotificationManager) getSystemService(NOTIFICATION_SERVICE))
+					.cancel(notificationId);
+		}
 	}
 
+	public void notifyUser(String message, Notification notification, int id,
+			boolean showTryAgain) {
+		notification.tickerText = message;
+		notification.contentView.setTextViewText(R.id.notification_text,
+				message);
+		if (showTryAgain) {
+			notification.contentView.setTextViewText(R.id.notification_title,
+					"Try again?");
+		}
+		((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(
+				id, notification);
+	}
 }
