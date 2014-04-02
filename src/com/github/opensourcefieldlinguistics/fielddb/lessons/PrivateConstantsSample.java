4e34a577e71b048c2fd9e1eb8e708f3a96f1fcca
cesine <cesine@yahoo.com>
2014-04-02 19:14:06 +0400

    updating private constant sample

--- src/com/github/opensourcefieldlinguistics/fielddb/lessons/PrivateConstantsSample.java
+++ src/com/github/opensourcefieldlinguistics/fielddb/lessons/PrivateConstantsSample.java
@@ -1,24 +1,26 @@
 package com.github.opensourcefieldlinguistics.fielddb.lessons;
 
+/**
+ * 
+ * http://developer.android.com/tools/devices/emulator.html#networkaddresses
+ * 10.0.2.2 emulator connection to localhost
+ */
 public class PrivateConstantsSample {
 
 	public static final String ACRA_PASS = "acouchdbpassword";
-	public static String ACRA_SERVER_URL = "https://10.0.2.2:6984/acra-learnx/_design/acra-storage/_update/report";
-
+	public static final String ACRA_SERVER_URL = "http://10.0.2.2:5984/acra-learnx/_design/acra-storage/_update/report";
 	public static final String ACRA_USER = "acouchdbusername";
 
-	public static final String DEFAULT_PUBLIC_USER_PASS = "none";
-	public static final String DEFAULT_PUBLIC_USERNAME = "public";
-
-	public static String DEFAULT_REGISTER_USER_URL = "https://10.0.2.2:3183/register";
-
-	// 10.0.2.2 emulator connection to localhost
-	// http://developer.android.com/tools/devices/emulator.html#networkaddresses
-	public static String DEFAULT_SERVER_URL = "https://10.0.2.2:6984";
+	// HTTPS localhost must restarting couchdb with the ssl pointing to this ip
+	// public static String DEFAULT_DATA_SERVER_URL = "https://10.0.2.2:6984";
+	public static final String DEFAULT_DATA_SERVER_URL = "http://10.0.2.2:5984";
+	public static final String DEFAULT_PUBLIC_USER_PASS = "ausername";
+	public static final String DEFAULT_PUBLIC_USERNAME = "apassword";
+	public static final String DEFAULT_REGISTER_USER_URL = "http://10.0.2.2:3183/register";
 
 	/*
-	 * Example on how to create a keystore from:
-	 * http://transoceanic.blogspot.com
+	 * Example on how to support self signed ssl certificates by creating a
+	 * keystore from: http://transoceanic.blogspot.com
 	 * /2011/11/android-import-ssl-certificate-and-use.html
 	 * 
 	 * keytool -importcert -v -trustcacerts -file ../acorpusserver.crt -alias
@@ -26,9 +28,6 @@ public class PrivateConstantsSample {
 	 * org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath
 	 * bcprov-jdk15on-150.jar -storetype BKS -storepass
 	 * "apasswordforyourkeystore"
-	 * 
-	 * Workaround for:
-	 * http://en.wikipedia.org/wiki/Server_Name_Indication#Support
 	 */
-	public static String KEYSTORE_PASS = "apasswordforyourkeystore";
+	public static final String KEYSTORE_PASS = "apasswordforyourkeystore";
 }
