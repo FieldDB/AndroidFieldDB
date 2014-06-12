c37b725cc76f822c563a8e879adecf265646fb5b
cesine <cesine@yahoo.com>
2014-06-12 18:33:23 +0400

    added a list pager which makes it possible to swipe between datum
    fixes https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/1533

---
+++ com/github/opensourcefieldlinguistics/fielddb/lessons/ui/ProductionExperimentViewPagerFragmentActivity.java
@@ -0,0 +1,67 @@
+package com.github.opensourcefieldlinguistics.fielddb.lessons.ui;
+
+import ca.ilanguage.oprime.Config;
+
+import com.github.opensourcefieldlinguistics.fielddb.database.DatumContentProvider;
+import com.github.opensourcefieldlinguistics.fielddb.database.DatumContentProvider.DatumTable;
+import com.github.opensourcefieldlinguistics.fielddb.speech.kartuli.R;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+public class ProductionExperimentViewPagerFragmentActivity
+		extends
+			FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+	private DatumFragmentPagerAdapter mPagerAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setContentView(R.layout.activity_production_experiment_datum_list);
+
+		this.initialisePaging();
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		String[] projection = {DatumTable.COLUMN_ID};
+		CursorLoader cursorLoader = new CursorLoader(this,
+				DatumContentProvider.CONTENT_URI, projection, null, null, null);
+		Cursor cursor = cursorLoader.loadInBackground();
+		this.mPagerAdapter.swapCursor(cursor);
+
+		return cursorLoader;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+		Log.d(Config.TAG, "Finished loading the ids for swipe paging");
+		this.mPagerAdapter.swapCursor(data);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		this.mPagerAdapter.swapCursor(null);
+	}
+
+	/**
+	 * Initialise the fragments to be paged
+	 */
+	private void initialisePaging() {
+
+		this.mPagerAdapter = new DatumFragmentPagerAdapter(
+				super.getSupportFragmentManager());
+		this.onCreateLoader(0, null);
+
+		ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
+		pager.setAdapter(this.mPagerAdapter);
+	}
+
+}
