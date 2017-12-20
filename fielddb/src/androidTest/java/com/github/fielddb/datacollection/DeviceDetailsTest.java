23df957a647432ea3b0759cd7a34af7af1fbc6aa
cesine <cesine@yahoo.com>
2017-12-20 16:16:03 +0000

    added support for android 6 runtime permissions in device details

--- fielddb/src/androidTest/java/com/github/fielddb/datacollection/DeviceDetailsTest.java
+++ fielddb/src/androidTest/java/com/github/fielddb/datacollection/DeviceDetailsTest.java
@@ -4,12 +4,12 @@ import android.support.test.filters.MediumTest;
 import android.support.test.filters.RequiresDevice;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -23,16 +23,23 @@ public class DeviceDetailsTest {
         super();
     }
 
-    @Before
-    public void setUp() throws Exception {
-        underTest = new DeviceDetails(getTargetContext());
-        assertThat(underTest, notNullValue());
-    }
-
     @Test
     public void setDeviceDetails_shouldWork() {
-        underTest.setDeviceDetails();
-        assertThat(underTest.mDeviceDetails, equalTo(" "));
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                underTest = new DeviceDetails(getTargetContext());
+                assertThat(underTest, notNullValue());
+                underTest.setDeviceDetails();
+                assertThat(underTest.mDeviceDetails, containsString("model: 'Nexus 5X'"));
+                assertThat(underTest.mDeviceDetails, containsString("sdk: '27'"));
+                assertThat(underTest.mDeviceDetails, containsString("identifier: 'aad6b1e07b3ec579'"));
+                assertThat(underTest.mDeviceDetails, containsString("location:{longitude: '0.0', latitude: '0.0', accuracy: '0.0'}"));
+                assertThat(underTest.mDeviceDetails, containsString("wifiMACaddress: 'unknown'"));
+                assertThat(underTest.mDeviceDetails, containsString("telephonyDeviceId:'unknown'"));
+                // assertThat(underTest.mDeviceDetails, containsString("{name: 'LG Nexus 5X', model: 'Nexus 5X', product: 'bullhead', manufacturer: 'LG', appversion: 'null', sdk: '27', osversion: '3.10.73-g7d1e90244638(4448085)',device: 'bullhead', screen: {height: '1794', width: '1080', ratio: '1.0', currentOrientation: 'portrait'}, serial: '00e77591894820b4', identifier: 'aad6b1e07b3ec579', wifiMACaddress: 'unknown', timestamp: '1513786005317',location:{longitude: '0.0', latitude: '0.0', accuracy: '0.0'} , telephonyDeviceId:'unknown'}"));
+            }
+        });
     }
 
 }
