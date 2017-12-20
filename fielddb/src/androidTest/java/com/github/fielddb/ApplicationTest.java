cb7457425cf24146377622cb62e618d8736acab6
cesine <cesine@yahoo.com>
2017-12-20 13:29:10 +0000

    updating to gradle 4.x and target 27
    scaffolding test case

--- fielddb/src/androidTest/java/com/github/fielddb/ApplicationTest.java
+++ fielddb/src/androidTest/java/com/github/fielddb/ApplicationTest.java
@@ -1,13 +1,39 @@
 package com.github.fielddb;
 
-import android.app.Application;
+import android.content.Context;
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.*;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertThat;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class ApplicationTest extends ApplicationTestCase<FieldDBApplication> {
+    private Context mContext;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(FieldDBApplication.class);
     }
+
+    @Before
+    public void initTargetContext() {
+        mContext = getTargetContext();
+        assertThat(mContext, notNullValue());
+    }
+
+    @Test
+    public void mUserIsDefined() {
+        assertThat(mContext, notNullValue());
+    }
+
 }
