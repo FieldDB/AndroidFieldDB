3ee5a368fda781441d56d435e6837d8543c5fe83
cesine <cesine@yahoo.com>
2017-12-27 18:22:02 +0100

    not running test on main thread
    java.lang.NoSuchMethodError: No virtual method toPath()Ljava/nio/file/Path; in class Ljava/io/File; or its super classes (declaration of 'java.io.File' appears in /system/framework/core-oj.jar)
    at com.github.fielddb.datacollection.SecureHttpClient.addFilePart(SecureHttpClient.java:76)
    at com.github.fielddb.service.UploadAudioVideoService.upload(UploadAudioVideoService.java:142)
    at com.github.fielddb.service.UploadAudioVideoServiceTest.uploadShouldWork(UploadAudioVideoServiceTest.java:55)
    at java.lang.reflect.Method.invoke(Native Method)
    at org.junit.runners.model.FrameworkMethod.runReflectiveCall(FrameworkMethod.java:50)
    at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
    at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)
    at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
    at android.support.test.rule.GrantPermissionRule.evaluate(GrantPermissionRule.java:109)
    at android.support.test.rule.GrantPermissionRule.evaluate(GrantPermissionRule.java:109)
    at android.support.test.rule.GrantPermissionRule.evaluate(GrantPermissionRule.java:109)
    at org.junit.rules.RunRules.evaluate(RunRules.java:20)
    at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)
    at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)
    at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)
    at org.junit.runners.ParentRunner.run(ParentRunner.java:290)
    at org.junit.runners.ParentRunner.schedule(ParentRunner.java:71)
    at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)
    at org.junit.runners.ParentRunner.access-bash00(ParentRunner.java:58)
    at org.junit.runners.ParentRunner.evaluate(ParentRunner.java:268)
    at org.junit.runners.ParentRunner.run(ParentRunner.javaat org.junit.runners.ParentRunner.run(ParentRunner.javaat org.junit.runners.ParentRunner.run(ParentRunner.javaatorg.junit.runners.ParentRunner.run(ParentRunner.java:290)
    at org.junit.runners.ParentRunner.schedule(ParentRunner.java:71)
    at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)
    at org.junit.runners.ParentRunner.access-bash00(ParentRunner.java:58)
    at org.junit.runners.ParentRunner.evaluate(ParentRunner.java:268)
    at org.junit.runners.ParentRunner.run(ParentRunner.java:363)
    at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
    at org.junit.runner.JUnitCore.run(JUnitCore.java:115)
    at android.support.test.internal.runner.TestExecutor.execute(TestExecutor.java:58)
    at android.support.test.runner.AndroidJUnitRunner.onStart(AndroidJUnitRunner.java:375)
    at android.app.Instrumentation.run(Instrumentation.java:1932)

--- fielddb/src/androidTest/java/com/github/fielddb/service/UploadAudioVideoServiceTest.java
+++ fielddb/src/androidTest/java/com/github/fielddb/service/UploadAudioVideoServiceTest.java
@@ -1,29 +1,16 @@
 package com.github.fielddb.service;
 
-import android.app.Activity;
-import android.app.Instrumentation.ActivityResult;
-import android.content.Intent;
 import android.net.Uri;
 import android.support.test.filters.LargeTest;
-import android.support.test.filters.RequiresDevice;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.GrantPermissionRule;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
@@ -42,22 +29,11 @@ public class UploadAudioVideoServiceTest {
   @Rule
   public GrantPermissionRule mRuntimeAccessWifiStatePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_WIFI_STATE);
 
-//  @Before
-//  public void stubAllExternalIntents() {
-//    ActivityResult ar = new ActivityResult(Activity.RESULT_OK, null);
-//    intending(not(isInternal())).respondWith(ar);
-//  }
-
   @Test
   public void uploadShouldWork() {
-    getInstrumentation().runOnMainSync(new Runnable() {
-      @Override
-      public void run() {
-        underTest = new UploadAudioVideoService();
-        assertThat(underTest, notNullValue());
-        String response = underTest.upload(Uri.parse("/sdcard/1.raw"));
-        assertThat(response, containsString(" something "));
-      }
-    });
+    underTest = new UploadAudioVideoService();
+    assertThat(underTest, notNullValue());
+    String response = underTest.upload(Uri.parse("/sdcard/1.raw"));
+    assertThat(response, containsString(" something "));
   }
 }
