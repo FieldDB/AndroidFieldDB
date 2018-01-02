74aa9f282b2606878b9dbc7765cf6b1119964734
cesine <cesine@yahoo.com>
2018-01-02 11:06:32 +0000

    using ActivityTestRule works

--- fielddb/src/androidTest/java/com/github/fielddb/lessons/ui/DatumListActivityTest.java
+++ fielddb/src/androidTest/java/com/github/fielddb/lessons/ui/DatumListActivityTest.java
@@ -1,46 +1,31 @@
 package com.github.fielddb.lessons.ui;
 
-import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
 
-import com.github.fielddb.test.R;
+import com.github.fielddb.MainActivity;
 
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 
 @RunWith(AndroidJUnit4.class)
 @MediumTest
-public class DatumListActivityTest extends ActivityInstrumentationTestCase2<DatumListActivity> {
-  DatumListActivity mActivity;
+public class DatumListActivityTest {
+  MainActivity mActivity;
 
-  @Before
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-    getTargetContext().getApplicationContext().setTheme(R.style.AppTheme);
-
-    injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-    getInstrumentation().setInTouchMode(false);
-    setActivityInitialTouchMode(false);
-
-    mActivity = getActivity();
-  }
-
-  public DatumListActivityTest() {
-    super(DatumListActivity.class);
-  }
+  @Rule
+  public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
   @Test
   public void onCreate() {
-    assertThat(mActivity, notNullValue());
+    mActivity = mActivityRule.getActivity();
+    assertThat(mActivityRule.getActivity(), notNullValue());
   }
 
   @Test
