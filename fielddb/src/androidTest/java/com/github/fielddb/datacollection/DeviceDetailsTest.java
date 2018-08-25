651aa2fd35bf3975e516c48c359ce8406b34e9d7
cesine <cesine@yahoo.com>
2018-08-25 11:52:26 -0400

    update device details test to latest testing device

--- fielddb/src/androidTest/java/com/github/fielddb/datacollection/DeviceDetailsTest.java
+++ fielddb/src/androidTest/java/com/github/fielddb/datacollection/DeviceDetailsTest.java
@@ -31,11 +31,11 @@ public class DeviceDetailsTest {
                 underTest = new DeviceDetails(getTargetContext());
                 assertThat(underTest, notNullValue());
                 underTest.setDeviceDetails();
-                assertThat(underTest.mDeviceDetails, containsString("model: 'Nexus 5X'"));
+                assertThat(underTest.mDeviceDetails, containsString("model: 'Pixel 2'"));
                 assertThat(underTest.mDeviceDetails, containsString("sdk: '27'"));
-                assertThat(underTest.mDeviceDetails, containsString("identifier: 'aad6b1e07b3ec579'"));
+                assertThat(underTest.mDeviceDetails, containsString("identifier: '63bac898cd404a04'"));
                 assertThat(underTest.mDeviceDetails, containsString("location:{longitude: '0.0', latitude: '0.0', accuracy: '0.0'}"));
-                assertThat(underTest.mDeviceDetails, containsString("wifiMACaddress: 'unknown'"));
+                assertThat(underTest.mDeviceDetails, containsString("wifiMACaddress: '02:00:00:00:00:00'"));
                 assertThat(underTest.mDeviceDetails, containsString("telephonyDeviceId:'unknown'"));
                 // assertThat(underTest.mDeviceDetails, containsString("{name: 'LG Nexus 5X', model: 'Nexus 5X', product: 'bullhead', manufacturer: 'LG', appversion: 'null', sdk: '27', osversion: '3.10.73-g7d1e90244638(4448085)',device: 'bullhead', screen: {height: '1794', width: '1080', ratio: '1.0', currentOrientation: 'portrait'}, serial: '00e77591894820b4', identifier: 'aad6b1e07b3ec579', wifiMACaddress: 'unknown', timestamp: '1513786005317',location:{longitude: '0.0', latitude: '0.0', accuracy: '0.0'} , telephonyDeviceId:'unknown'}"));
             }
