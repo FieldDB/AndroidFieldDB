9e75143a11c803aec3ae9452f7f9727756c52b5c
cesine <cesine@yahoo.com>
2018-08-27 23:18:24 -0400

    using main activity as sample accounts
    integration

--- fielddb/src/main/java/com/github/fielddb/MainActivity.java
+++ fielddb/src/main/java/com/github/fielddb/MainActivity.java
@@ -1,14 +1,55 @@
 package com.github.fielddb;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.github.fielddb.R;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends FragmentActivity {
+  // Content provider scheme
+  public static final String SCHEME = "content://";
+  // The authority for the sync adapter's content provider
+  public static final String AUTHORITY = "com.github.fielddb.default.datum";
+  // An account type, in the form of a domain name
+  public static final String ACCOUNT_TYPE = "com.github.fielddb";
+  // The account name
+  public static final String ACCOUNT = "public";
+
+  Account mAccount;
+  ContentResolver mResolver;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mAccount = CreateSyncAccount(this);
+
+    Bundle settingsBundle = new Bundle();
+    settingsBundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+    settingsBundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+    ContentResolver.requestSync(mAccount, AUTHORITY, settingsBundle);
+  }
+
+  /**
+   * Create a new dummy account for the sync adapter
+   *
+   * @param context The application context
+   */
+  public static Account CreateSyncAccount(Context context) {
+    // Create the account type and default account
+    Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+    // Get an instance of the Android account manager
+    AccountManager accountManager = (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+    /*
+     * Add the account and account type, no password or user data
+     */
+    accountManager.addAccountExplicitly(newAccount, "none", null);
+
+    return newAccount;
+  }
 }
